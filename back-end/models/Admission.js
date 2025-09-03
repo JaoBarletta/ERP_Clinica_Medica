2f478aa03d42d56012b29ed2e328a67542257fc4
GabrielKall <gabrielkallmeyer@gmail.com>
2025-09-02 22:07:24 -0300

    Classes e construtores criados.

---
+++ back-end/models/Admission.js
@@ -0,0 +1,10 @@
+class Admission{
+    constructor(id, code, ward, status){
+        this.id = id;
+        this.code = code;
+        this.ward = ward;
+        this.status = status;
+    }
+}
+
+module.exports = Admission;
---
+++ back-end/models/Exam.js
@@ -0,0 +1,10 @@
+class Exam{
+    constructor(id, appointment_id, type, status, result_url, result_text){
+        this.id = id;
+        this.appointment_id = appointment_id;
+        this.type = type;
+        this.status = status;
+        this.result_url = result_url;
+        this.result_text = result_text;
+    }
+}
